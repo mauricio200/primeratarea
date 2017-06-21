09a3fba90fab2f353c6c0998a1a021745bd48feb
mauricio zapata <torredemago123@gmail.com>
2017-06-21 03:50:26 -0500

    tercer trabajo

---
+++ srCc/VisorDeFuentes/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package VisorDeFuentes;
+
+import gui.MainForm;
+
+/**
+ *
+ * @author Geronimo
+ */
+public class Main {
+
+    public static void main(String [] args){
+    MainForm vent = new MainForm();
+    vent.setVisible(true);
+            
+    }
+    
+}
